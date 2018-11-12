994a77e712f9434dbbb73ce54ee16adc6354b916
mercuriete <mercuriete@gmail.com>
2018-11-12 17:51:45 +0100

    working on #6 Cleaning some ui elements from boilerplate

--- app/src/main/java/org/mercuriete/musiciantools/MenuActivity.kt
+++ app/src/main/java/org/mercuriete/musiciantools/MenuActivity.kt
@@ -17,29 +17,9 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-        button.setOnClickListener { view ->
+        button.setOnClickListener { _ ->
             bpmCalculatorService.beat()
             bpmText.text = String.format("%.2f", bpmCalculatorService.getBPM())
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 }
