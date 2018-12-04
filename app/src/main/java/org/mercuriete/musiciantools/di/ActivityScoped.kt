5e6286c9c4412097bff6f9a82b3b2348c42949ad
mercuriete <mercuriete@gmail.com>
2018-12-04 18:09:22 +0100

    adding new fragment scope and fix annotation deprecation

--- app/src/main/java/org/mercuriete/musiciantools/di/ActivityScoped.kt
+++ app/src/main/java/org/mercuriete/musiciantools/di/ActivityScoped.kt
@@ -1,10 +1,6 @@
 package org.mercuriete.musiciantools.di
 
-import java.lang.annotation.Documented
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 import javax.inject.Scope
-
 
 /**
  * In Dagger, an unscoped component cannot depend on a scoped component. As
@@ -12,7 +8,6 @@ import javax.inject.Scope
  * scope to be used by all fragment components. Additionally, a component with a specific scope
  * cannot have a sub component with the same scope.
  */
-@Documented
 @Scope
-@Retention(RetentionPolicy.RUNTIME)
+@kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 annotation class ActivityScoped
