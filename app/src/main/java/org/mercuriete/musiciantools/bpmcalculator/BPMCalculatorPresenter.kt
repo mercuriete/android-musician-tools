4d3e4ffebef5eb591f84b1588fc1f8631c6e85da
mercuriete <mercuriete@yahoo.es>
2019-07-20 19:25:42 +0100

    Update to new android sdk 29 (#19)

--- app/src/main/java/org/mercuriete/musiciantools/bpmcalculator/BPMCalculatorPresenter.kt
+++ app/src/main/java/org/mercuriete/musiciantools/bpmcalculator/BPMCalculatorPresenter.kt
@@ -1,6 +1,6 @@
 package org.mercuriete.musiciantools.bpmcalculator
 
-import android.support.annotation.Nullable
+import androidx.annotation.Nullable
 import javax.inject.Inject
 
 class BPMCalculatorPresenter @Inject constructor(private var bpmCalculatorService: BPMCalculatorService) : BPMCalculatorContract.Presenter {
