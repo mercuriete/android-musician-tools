c3934c9db436d35cc64ea9455292558a4d209863
mercuriete <mercuriete@gmail.com>
2021-02-04 22:59:12 +0000

    fix: Migrate from deprecated kotlin android extensions to view bindings

--- app/src/main/java/org/mercuriete/musiciantools/MainActivity.kt
+++ app/src/main/java/org/mercuriete/musiciantools/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.main_activity.*
+import org.mercuriete.musiciantools.databinding.MainActivityBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -13,9 +13,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.main_activity)
+        val binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navController = Navigation.findNavController(this, R.id.content)
-        NavigationUI.setupWithNavController(navigation, navController)
+        NavigationUI.setupWithNavController(binding.navigation, navController)
     }
 }
