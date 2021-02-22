6e570a3dca21b84dbe9afc6297578900153f48fb
mercuriete <mercuriete@gmail.com>
2021-02-22 18:54:36 +0000

    fix: Sonar Party!
    * Closes #128

--- app/src/main/java/org/mercuriete/musiciantools/aboutme/AboutMeFragment.kt
+++ app/src/main/java/org/mercuriete/musiciantools/aboutme/AboutMeFragment.kt
@@ -1,21 +1,9 @@
 package org.mercuriete.musiciantools.aboutme
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
-import org.mercuriete.musiciantools.R
+import org.mercuriete.musiciantools.R.layout.about_me_fragment
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class AboutMeFragment @Inject constructor() : Fragment(), AboutMeContract.View {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.about_me_fragment, container, false)
-    }
-}
+class AboutMeFragment @Inject constructor() : Fragment(about_me_fragment), AboutMeContract.View
