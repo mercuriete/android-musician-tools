8e63e5d4efa18071c2ad0620bd95e38d49ad4399
mercuriete <mercuriete@gmail.com>
2021-02-22 20:58:18 +0000

    test: Implement a generic way to assert on TextView

--- app/src/androidTest/java/org/mercuriete/musiciantools/test/MainActivityScreenRobot.kt
+++ app/src/androidTest/java/org/mercuriete/musiciantools/test/MainActivityScreenRobot.kt
@@ -2,14 +2,9 @@ package org.mercuriete.musiciantools.test
 
 import androidx.test.core.app.launchActivity
 import org.mercuriete.musiciantools.MainActivity
-import org.mercuriete.musiciantools.R
 
 class MainActivityScreenRobot : GenericRobot() {
     fun launchMainActivityScreen() {
         launchActivity<MainActivity>()
-    }
-
-    fun canSeeAMessageOnBpmText(text: String) {
-        genericAssertTextView(R.id.bpmText, text)
     }
 }
