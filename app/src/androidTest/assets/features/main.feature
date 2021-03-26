6c597926b5f6c1ef63c124ceaf36f14da66ac5dc
mercuriete <mercuriete@gmail.com>
2021-03-26 18:32:04 +0000

    test: UI tests should not reference internal ids
    * Closes #145
    * Changes espresso expressions to match text content

--- app/src/androidTest/assets/features/main.feature
+++ app/src/androidTest/assets/features/main.feature
@@ -2,4 +2,4 @@ Feature: Open application
 
     Scenario: Successful open application
         Given I start the application
-        Then I expect to see "Hello World!" text on "bpmText" TextView
+        Then I expect to see "Hello World!" text
--- app/src/androidTest/java/org/mercuriete/musiciantools/test/GenericRobot.kt
+++ app/src/androidTest/java/org/mercuriete/musiciantools/test/GenericRobot.kt
@@ -3,15 +3,25 @@ package org.mercuriete.musiciantools.test
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withResourceName
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
+import org.hamcrest.CoreMatchers.allOf
 
 open class GenericRobot {
-    fun genericAssertTextView(textViewResName: String, text: String) {
-        onView(withResourceName(textViewResName)).check(matches(withText(text)))
+    fun genericAssertTextView(text: String) {
+        onView(
+            allOf(
+                withText(text),
+                withEffectiveVisibility(Visibility.VISIBLE)
+            )
+        ).check(matches(isCompletelyDisplayed()))
     }
 
-    fun genericClickOnView(resName: String) {
-        onView(withResourceName(resName)).perform(click())
+    fun genericClickOnView(text: String) {
+        onView(
+            allOf(
+                withText(text),
+                withEffectiveVisibility(Visibility.VISIBLE)
+            )
+        ).check(matches(isCompletelyDisplayed())).perform(click())
     }
 }
